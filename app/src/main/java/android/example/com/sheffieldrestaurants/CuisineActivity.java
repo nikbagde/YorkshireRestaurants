c23b2621eeb0df1397aab6a990f42814e6e1a6eb
Abbie <abbieturner95@hotmail.com>
2018-05-11 10:22:21 +0100

    upload commit

---
+++ app/src/main/java/android/example/com/sheffieldrestaurants/CuisineActivity.java
@@ -0,0 +1,13 @@
+package android.example.com.sheffieldrestaurants;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CuisineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuisine);
+    }
+}
---
+++ app/src/main/java/android/example/com/sheffieldrestaurants/MainActivity.java
@@ -0,0 +1,44 @@
+package android.example.com.sheffieldrestaurants;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.transition.Explode;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.login_btn) Button login_btn;
+    @BindView(R.id.signin_btn) Button signin_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View view) {
+                setEnterExitTransition(new Intent(MainActivity.this,CuisineActivity.class));
+            }
+        });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void setEnterExitTransition(Intent intent){
+        getWindow().setExitTransition(new Explode().setDuration(800));
+        getWindow().setReenterTransition(new Explode().setDuration(800));
+        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(MainActivity.this).toBundle());
+    }
+}
