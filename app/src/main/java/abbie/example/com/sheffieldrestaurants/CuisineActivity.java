eb26096abb6a53fa89e4461ee14fb20180a81474
Abbie <abbieturner95@hotmail.com>
2018-05-11 11:46:41 +0100

    Added Cuisine Activity + Favorties Activity (basic xml added)

--- app/src/main/java/abbie/example/com/sheffieldrestaurants/CuisineActivity.java
+++ app/src/main/java/abbie/example/com/sheffieldrestaurants/CuisineActivity.java
@@ -1,13 +1,49 @@
 package abbie.example.com.sheffieldrestaurants;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class CuisineActivity extends AppCompatActivity {
+
+    @BindView(R.id.adView) AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cuisine);
+        ButterKnife.bind(this);
+
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.favorites_menu:
+                startActivity(new Intent(this, FavoritesActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/abbie/example/com/sheffieldrestaurants/MainActivity.java
+++ app/src/main/java/abbie/example/com/sheffieldrestaurants/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
         ButterKnife.bind(this);
 
         login_btn.setOnClickListener(new View.OnClickListener() {
